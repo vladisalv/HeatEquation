6c5f436e4cca68388d5287206bcff5e5bd88ad18
vladisalv <vladisalv@yandex.ru>
2014-05-27 08:03:56 +0400

    grid. this is template and for two task

--- include/grid.h
+++ include/grid.h
@@ -3,8 +3,24 @@
 
 #include "types.h"
 
+#include <vector>
+
+using namespace std;
+
 class Grid {
-    grid_t grid;
+    grid_vec *grid;
+    int gridRow, gridCol;
+    double left, right, time, time_step, coor_step;
+
+    void initGrid();
+    void leftBoundaryGrid();
+    void rightBoundaryGrid();
+public:
+    Grid(double left, double right, double time, double time_step, double coor_step);
+    ~Grid();
+
+    virtual void solveEquation() = 0;
+    virtual void writePlot() = 0;
 };
 
 #endif /* __GRID_HEADER__ */ 
--- src/grid.cpp
+++ src/grid.cpp
@@ -1,1 +1,12 @@
 #include "grid.h"
+
+Grid::Grid(double new_left, double new_right,
+    double new_time, double new_time_step, double new_coor_step)
+    : time(new_time), time_step(new_time_step), coor_step(new_coor_step),
+    left(new_left), right(new_right)
+{
+    gridRow = (int)((right - left) / coor_step);
+    gridCol = (int)((time - 0) / time_step);
+
+    grid = new grid_vec (gridRow, vector<grid_t>(gridCol, 0));
+}
